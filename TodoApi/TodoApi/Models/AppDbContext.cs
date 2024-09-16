31152533bb5731656a3357acb70e65391e50e173
José Luiz <redg7338@gmail.com>
2024-09-16 18:19:55 -0300

    Adding Database

--- TodoApi/TodoApi/Models/AppDbContext.cs
+++ TodoApi/TodoApi/Models/AppDbContext.cs
@@ -6,7 +6,9 @@ namespace TodoApi.Models
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Author> Authors { get; set; } //essa é a "forma" para criar tabelas no banco que for conectado. Eu estou dizendo que quero
+        public DbSet<Book> Books { get; set; }    //criar uma tabela chamada Authors, e suas colunas terão todas as propriedades do model Author.
     }
 }
